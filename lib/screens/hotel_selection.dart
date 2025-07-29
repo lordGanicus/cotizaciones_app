39c09d6535b65abaa7dfda39c96a68fb946d115f
lordGanicus <ribertxdxd@gmail.com>
2025-07-29 17:57:21 -0400

    cambios en el diseño

--- lib/screens/hotel_selection.dart
+++ lib/screens/hotel_selection.dart
@@ -95,8 +95,9 @@ class _HotelSelectionPageState extends State<HotelSelectionPage> {
           });
         }
       }
-
+      
       // Cargar cotizaciones con más detalle
+
       await _cargarCotizaciones();
 
       setState(() {
